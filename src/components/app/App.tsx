51e3e795709f57a187af78dfc803095f033d3967
Niv Shani <nivshani08@gmail.com>
2020-10-09 15:22:18 +0300

    Header component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,27 +1,15 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
-import "./App.css";
+import "./App.scss";
+import {Header} from "../header";
 import meteors from "../../assets/meteors.json";
 import MeteorsSearcher from "../../utils/MeteorsSearcher";
 
 const App = () => {
     console.log(new MeteorsSearcher(meteors));
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="app">
+            <Header title="MeteorsQuerist"
+                    subtitle="meteors are falling as we speak" />
         </div>
     );
 };
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./header.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMeteor} from "@fortawesome/free-solid-svg-icons";
+
+interface Props {
+    title: string;
+    subtitle: string;
+}
+
+const Header = (props: Props) => (
+    <div className="header-wrapper">
+        <FontAwesomeIcon icon={faMeteor} className="icon" />
+        <span className="title" title={props.title}>{ props.title }</span>
+        <span className="subtitle">{ props.subtitle }</span>
+    </div>
+);
+
+export default Header;
