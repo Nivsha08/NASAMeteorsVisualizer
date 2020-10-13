4052659f6f1fa4d6213a012200f4142ec1ce730c
Niv Shani <nivshani08@gmail.com>
2020-10-13 22:34:28 +0300

    build for deployment

---
+++ build/precache-manifest.10182ed6e36b1698c55e52346ac7fdde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecb2bd4337465cf5210749f32e23204",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11f6f1eb185465ab5ca6",
+    "url": "/static/css/2.375dbf40.chunk.css"
+  },
+  {
+    "revision": "f66bddd04f9a4171b7ca",
+    "url": "/static/css/main.0d9bb573.chunk.css"
+  },
+  {
+    "revision": "11f6f1eb185465ab5ca6",
+    "url": "/static/js/2.0b04b434.chunk.js"
+  },
+  {
+    "revision": "0daae68c6a61b992753487cac1b227a2",
+    "url": "/static/js/2.0b04b434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66bddd04f9a4171b7ca",
+    "url": "/static/js/main.c33f7c22.chunk.js"
+  },
+  {
+    "revision": "6a95f797858447187583",
+    "url": "/static/js/runtime-main.16dc895c.js"
+  }
+]);
